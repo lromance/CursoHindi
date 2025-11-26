f87e0eefe45f0d79d776c0b149d7a585020c7e55
Your Name <you@example.com>
2025-11-26 23:54:36 +0100

    Fix: Move static content to public folder for GitHub Pages compatibility

--- src/pages/CoursePage.jsx
+++ src/pages/CoursePage.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import HtmlViewer from '../components/HtmlViewer';
 
 export default function CoursePage() {
-  return <HtmlViewer file="/src/content/curso_completo.html" />;
+  return <HtmlViewer file={`${import.meta.env.BASE_URL}content/curso_completo.html`} />;
 }
--- src/pages/ProgramPage.jsx
+++ src/pages/ProgramPage.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import HtmlViewer from '../components/HtmlViewer';
 
 export default function ProgramPage() {
-  return <HtmlViewer file="/src/content/programacion.html" />;
+  return <HtmlViewer file={`${import.meta.env.BASE_URL}content/programacion.html`} />;
 }
